2f89cdfc51dec6eac8d6459476a7b9a9913c0ecd
JaiLaDalleDeOufMgl <jailadalledeoufmgl@proton.me>
2023-11-25 23:25:09 +0100

    Firt Commit

---
+++ discord_self_embed/utils/embed.py
@@ -0,0 +1,40 @@
+import urllib.parse
+
+class Embed:
+    def __init__(self) -> None:
+        self.hide_text = "||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||"
+        self.base_url = "https://appembed.netlify.app/e?"
+        self.params = {}
+
+    def __str__(self) -> str:
+        return self.generate_url(hide_url=True)
+
+    def set_title(self, title) -> None:
+        self.params["author"] = title
+
+    def set_title_url(self, url) -> None:
+        self.params["author_url"] = url
+
+    def set_description(self, description) -> None:
+        self.params["description"] = description
+
+    def set_color(self, color) -> None:
+        self.params["color"] = color
+
+    def set_author(self, name) -> None:
+        self.params["provider"] = name
+
+    def set_author_url(self, url) -> None:
+        self.params['provider_url'] = url
+
+    def set_image(self, url) -> None:
+        self.params["image"] = url
+
+    def generate_url(self, *, hide_url=False, shorten_url=True, shortener=None) -> str:
+        for key in list(self.params.keys()):
+            if self.params[key] == "" or self.params[key] is None:
+                del self.params[key]
+
+        url = self.base_url + urllib.parse.urlencode(self.params)
+
+        return self.hide_text + " " + url
---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='discord_self_embed',
+    packages=['discord_self_embed', 'discord_self_embed.utils'],
+    version='0.0.1',
+    license='MIT',
+    description='A way for selfbots to send embeds again.',
+    author='JaiFaim',
+    author_email='jailadalledeoufmgl@proton.me',
+    url='https://github.com/JaiLaDalleDeOufMgl/discord.py-self_embed',
+    download_url='',
+    keywords=['discord', 'embed', 'selfbot', "discord embed",
+              "embed discord", "discord selfbot embed", "embed selfbot discord"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
+
